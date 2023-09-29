818d384ce4e7898ca13d90f62fa60e1940ae8471
abrack-gh <a.brack@live.co.uk>
2023-09-29 16:01:14 +0100

    ExpresJS impl

--- app.js
+++ app.js
@@ -1,57 +1,35 @@
 //Create a server...
 const http = require('http');
-const fs = require('fs')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    // process.exit();
+const application = express();
 
-    const url = req.url;
-    const method = req.method;
+//Parser
+application.use(bodyParser.urlencoded({extended: false}));
 
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-
-        return res.end;
-    }
-
-    if(url === "/message" && method === "POST"){
-        //take user input as a chunk and push into empty array
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk);
-        });
-        //Buffer the chunks
-        return req.on('end', () => {
-           const parsedBody = Buffer.concat(body).toString();
-           const message = parsedBody.split('=')[1];
-           fs.writeFile('message.txt', message, (error) => {
-
-               //Create file for above user input on form
-               //Send redirect status code in response
-               res.statusCode = 302;
-               //redirect to '/'
-               res.setHeader('Location', '/');
-               return res.end();
-
-           });
-        });
-
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello world!</h1></body>');
-    res.write('</html>');
-    res.end();
+application.use('/', (req, res, next) => {
+    console.log("This middleware always runs!");
+    next();
 });
 
-//Listen for requests on localhost:9000
-server.listen(9000, "localhost");
+application.use('/add', (req, res, next) => {
+    console.log("Second middleware!");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+//Filters only for post requests
+application.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+application.use('/', (req, res, next) => {
+    console.log("Third middleware!");
+    res.send('<h1>Hello!</h1>');
+});
+
+//Spin up server, and connect to routes.js module
+application.listen(9000, 'localhost');
 
 
