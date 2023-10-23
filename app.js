cbf122343024794a2ce3bb92030904e268e7e3da
abrack-gh <a.brack@live.co.uk>
2023-10-23 16:48:33 +0100

    Sequelize changes to shopController and product model.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes = require('./routes/shop');
 const path = require('path');
 const application = express();
 const errorController = require('./controllers/errorController');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 application.set('view engine', 'ejs');
 // application.set('views', 'views');
@@ -22,7 +22,18 @@ application.use(express.static(path.join(__dirname, 'public')));
 //SEND ERROR VIEW WITH 404 ERROR IN HEADER
 application.use(errorController.getErrorPage);
 
+sequelize.sync()
+    .then(result => {
+    // console.log(result)
+    application.listen(9000, 'localhost');
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
 //Spin up server, and connect to routes.js module
-application.listen(9000, 'localhost');
+
+
+
 
 
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -36,16 +36,24 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
+    const id = req.body.id;
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(null, title, imageUrl, price, description);
-    product.save()
-        .then(() => {
-            res.redirect('/products');
-        })
-        .catch(err => console.log(err));
+    Product.create({
+        id: id,
+        title: title,
+        price: price,
+        imageUrl: imageUrl,
+        description: description
+    }).then(result => {
+        console.log(result);
+    })
+
+        .catch(err => {
+        console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
